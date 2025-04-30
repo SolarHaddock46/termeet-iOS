05517c81a43f9193bdb4e814cd25651aa498d2b3
Мацнев Владимир Дмитриевич <vdmatsnev@edu.hse.ru>
2025-04-30 15:01:00 +0300

    created project

---
+++ Termeet/TermeetTests/TermeetTests.swift
@@ -0,0 +1,17 @@
+//
+//  TermeetTests.swift
+//  TermeetTests
+//
+//  Created by Владимир Мацнев on 30.04.2025.
+//
+
+import Testing
+@testable import Termeet
+
+struct TermeetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
